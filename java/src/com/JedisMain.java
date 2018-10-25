f8a15f34fa8c7150df177de4a56c6fd3b07aec1b
lostjc <lostjc@ibizsoftware.net>
2018-10-25 15:39:00 +0900

    커밋

---
+++ java/src/com/JedisMain.java
@@ -0,0 +1,75 @@
+package com;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisMain {
+	public static void main(String[] args) {
+		String host = "103.75.0.18";
+        int port = 6379;
+        int timeout = 3000;
+        int db = 0;
+        
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        
+        JedisPool pool = new JedisPool(jedisPoolConfig, host, port, timeout, null, db);
+        
+        Jedis jedis = pool.getResource();
+        
+        //Connect 체크 
+        System.out.println(jedis.isConnected());
+        
+        jedis.set("test:test", "1");
+        
+        System.out.println( jedis.get("test:test") );
+        
+//        String key = "99:98";
+        
+//        jedis.lpush(key, "1");
+        
+//        jedis.set("key4", "6");
+//        jedis.set("key5", "6");
+//        
+//        // 데이터의 만료시간을 지정
+//        jedis.expire("key5",1);
+        
+//        String key = "99:98";
+        
+//        int i = 0;
+//        while( true ){
+//        	i++;
+//        	jedis.lpush(key, String.valueOf(i));
+//        	
+//        	if( i == 1000 ){
+//        		break;
+//        	}
+//        }
+        
+//        jedis.lpush(key, String.valueOf(4));
+        
+//        System.out.println( jedis.rpop(key) );
+        
+//        String val = "";
+//        i = 0;
+//        while( null != (val = jedis.rpop(key)) ){
+//        	i++;
+//        	System.out.println(i);
+//        	System.out.println(val);
+//        }
+        
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    
+        if( jedis != null ){
+            jedis.close();
+        }
+        pool.close();
+        
+    }
+ 
+}
