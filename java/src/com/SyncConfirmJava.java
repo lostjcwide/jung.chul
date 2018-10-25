102090e5db124271c26c718ea682096aa0a19d06
lostjc <lostjc@ibizsoftware.net>
2018-10-25 14:20:02 +0900

    커밋

--- java/src/com/SyncConfirmJava.java
+++ java/src/com/SyncConfirmJava.java
@@ -6,10 +6,6 @@ public class SyncConfirmJava {
 	
 	public static void main(String[] args) {
 		String[] urls = {
-				"http://ec2-13-209-57-181.ap-northeast-2.compute.amazonaws.com:8540"
-				, "http://ec2-52-78-165-106.ap-northeast-2.compute.amazonaws.com:8540"
-				, "http://ec2-52-79-36-191.ap-northeast-2.compute.amazonaws.com:8540"
-				, "http://ec2-52-78-205-130.ap-northeast-2.compute.amazonaws.com:8540"
 				};
 		
 		for (String url : urls) {
