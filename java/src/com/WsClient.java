a2482c0312629fbd2b17e7765a20efe25351db84
lostjc <lostjc@ibizsoftware.net>
2018-10-30 17:18:19 +0900

    커밋 2018-10-30

--- java/src/com/WsClient.java
+++ java/src/com/WsClient.java
@@ -58,4 +58,49 @@ public class WsClient {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void call(String url, String jsonStr){
+		try {
+			// https://websocket.org/echo.html가 제공하는 WebSocket 에코 서버에서 기능 테스트
+			WebSocketClient webSocketClient = new WebSocketClient(new URI("ws://"+url), new Draft_10()) {
+
+			    @Override
+			    public void onOpen(ServerHandshake serverHandshake) {
+			        // WebSocket 서버 연결 후 동작 정의, 아래는 Hello 메시지 발송
+			    	JSONObject json = new JSONObject(jsonStr);
+			        this.send(json.toString().getBytes());
+			    }
+
+			    @Override
+			    public void onMessage(String message) {
+			        // WebSocket 서버에서 메시지 수신시 동작 정의, 아래는 Hello 메시지 수신시 연결 종료
+//			        System.out.println(message);
+			        JSONObject js = new JSONObject(message);
+			        System.out.println(url + " : "+js.toString());
+//			        System.out.println(url + " : " + Long.parseLong(js.getString("result").replace("0x", ""), 16));
+			    }
+
+			    @Override
+			    public void onClose(int code, String reason, boolean remote) {
+			        // 서버 연결 종료 후 동작 정의
+			    }
+
+			    @Override
+			    public void onError(Exception ex) {
+			        // 예외 발생시 동작 정의
+			    	ex.printStackTrace();
+			    }
+
+			};
+			// 앞서 정의한 WebSocket 서버에 연결한다.
+			webSocketClient.connect();
+			
+			Thread.sleep(3000);
+			
+			webSocketClient.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- java/src/com/WsMain.java
+++ java/src/com/WsMain.java
@@ -10,7 +10,7 @@ public class WsMain {
 		
 		
 		for (String url : urls) {
-			WsClient.call(url);
+			WsClient.call(url, "{\"method\":\"shh_getPrivateKey\",\"id\":\"1540884195933\",\"jsonrpc\":\"2.0\",\"params\":[\"0x1286c3e756113b569921383d0679ae17284b9ec065d4c9574f0358f3778e0f1b\"]}");
 		}
 		
 	}
